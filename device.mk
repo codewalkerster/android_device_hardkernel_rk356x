e489d39834dc41c30c8d45fa14cf7ddf88145d31
Luke go <sangch.go@gmail.com>
2023-06-15 14:16:21 +0900

    ODROID-M1: Add boblight stuffs.
    Signed-off-by: Luke go <sangch.go@gmail.com>
    Change-Id: I140c9704d28a3f0d8f27cc89a6833da98ac97792

--- device.mk
+++ device.mk
@@ -127,3 +127,6 @@ ifneq ("$(wildcard vendor/opengapps/build/opengapps-packages.mk)","")
     GAPPS_VARIANT := mini
     $(call inherit-product, vendor/opengapps/build/opengapps-packages.mk)
 endif
+
+# boblight
+$(call inherit-product-if-exists, $(LOCAL_PATH)/boblight.mk)
